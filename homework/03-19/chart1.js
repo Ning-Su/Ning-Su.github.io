8d6f46a3b106bd79b383990b31894e5e0b0e694e
Ning-Su <59713286+Ning-Su@users.noreply.github.com>
2020-03-19 17:03:05 -0400

    push

--- homework/03-19/chart1.js
+++ homework/03-19/chart1.js
@@ -1,10 +1,10 @@
-d3.select("#barchart")
+d3.select(".w3-image")
   .on("mousemove", function() {
     d3.select("#tooltip")
       .style("display", "block")
       .style("top", d3.event.pageY + 20 + "px")
       .style("left", d3.event.pageX + 20 + "px")
-      .html("Welcome to the first chart!");
+      .html("Welcome to my final project!");
   })
 
   .on("mouseout", function() {
