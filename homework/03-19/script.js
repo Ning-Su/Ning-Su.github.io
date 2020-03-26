be4873a6e9ba3d769a4c15f594d7c8ccf8450cf0
Ning-Su <59713286+Ning-Su@users.noreply.github.com>
2020-03-26 14:59:11 -0400

    push

--- homework/03-19/script.js
+++ homework/03-19/script.js
@@ -1,20 +1,74 @@
 
-var promises = [
-    d3.csv("./03-19/NYC wildland fire.csv",parseCSV),
-    d3.json("./03-19/nys.json")
-];
 
-Promise.all(promises).then(function(data) {
+    d3.csv("NYS wildland fire.csv", function(error, data) {
+        var WildfireData = data;
+        console.log(data);
+        });
+
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+
+   var svg = d3.select("#viz")
+   .attr("width", width)
+   .attr("height", height);
+
+   svg.select("#ocean")
+ .attr("width", width)
+ .attr("height", height);
+
+   var map = svg.select("#map");
 
 
-    console.log(data);
+   d3.json("nys.json", function(error, geoJSON) {
 
 
 
-    
+       var projection = d3.geoMercator()
+       .fitSize([width, height], geoJSON);
+
+       // var projection = d3.geoMiller()
+       // .fitSize([width, height], geoJSON);
+
+       var path = d3.geoPath()
+       .projection(projection);
+
+       var countries = map.selectAll("path")
+       .data(geoJSON.features);
+
+       countries.enter().append("path")
+       .attr("d", path)
+       .attr("fill", "green")
+       .attr("stroke","orange");
+
+       var points = [
+       {"name": "Boston", "coords": [-71.0589, 42.3601]},
+       {"name": "London", "coords": [-0.1278, 51.5074]}
+       ];
+
+       var circles = map.selectAll("circle")
+       .data(points);
+
+       circles.enter().append("circle")
+       .attr("transform", function(d){
+           return "translate(" + projection(d.coords) + ")";
+       })
+       .attr("r", 10)
+       .attr("fill", "white");
+
+       var zoom = d3.zoom()
+       .translateExtent([[0, 0], [width, height]])
+       .scaleExtent([1, 20])
+       .on("zoom", zoomed);
+
+       function zoomed() {
+       map.attr("transform", d3.event.transform);
+       }
+
+       svg.call(zoom);
+         // .on("dblclick.zoom",null);
 
 
+       
 
-
-
-});
+   
+   });
