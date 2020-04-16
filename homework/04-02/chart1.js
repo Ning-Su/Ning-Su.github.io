97f2eec7ed2fcf7b52fe0c4e7f8d764896ad1ef9
Ning-Su <59713286+Ning-Su@users.noreply.github.com>
2020-04-16 17:45:57 -0400

    push

--- homework/04-02/chart1.js
+++ homework/04-02/chart1.js
@@ -1,6 +1,6 @@
 d3.select(".w3-image")
   .on("mousemove", function() {
-    d3.select("#tooltip")
+    d3.select("#tooltip1")
       .style("display", "block")
       .style("top", d3.event.pageY + 20 + "px")
       .style("left", d3.event.pageX + 20 + "px")
@@ -10,7 +10,7 @@ d3.select(".w3-image")
   })
 
   .on("mouseout", function() {
-    d3.select("#tooltip")
+    d3.select("#tooltip1")
       .style("display", "none");
   });
 
